40402352f44e999cac31d3b4f1ca15395dfe175e
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2018-10-06 16:51:10 -0400

    adding my answers

--- pset2.js
+++ pset2.js
@@ -4,23 +4,37 @@
 // pleae create:
 // a variable called `helloWrold`, you can set it equal to whatever you want
 
+let helloWorld = 1
+
 // a variable called `this_is_snake_cased`, you can set it equal to whatever you want
 
+let this_is_snake_cased = 2
 
 // a variable called `thisVarHasNumbers111`, you can set it equal to whatever you want
 
+let thisVarHasNumbers111 = 3
 
 // 1. create a string - it should be your first name
 
+let myFirstName = "Nicole"
 
 // 2. create another string - it should be your last name
 
+let myLastName = 'Hopkins'
 
 // 3. create a third string using the first two strings you've defined
+
+let myFullName = myFirstName + " " + myLastName
+
+
 // it should read [FIRST NAME][SPACE][LAST NAME]
 
 // 4. create a string - it should be your middle name
 
+let myMiddleName = "Lynn"
 
 // 5. redefine the string in step 3. so that it now shows 
 // [FIRST NAME][SPACE][MIDDLE NAME][SPACE][LAST NAME]
+
+myFullName = myFirstName + " " + myMiddleName + " " + myLastName
+console.log(myFullName)
